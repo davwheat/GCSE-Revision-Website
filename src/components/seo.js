0344afc88f3b3e2b36bb50691ad5e606e1106ed9
David Wheatley <blokmaster.help@gmail.com>
2019-09-20 02:56:54 +0100

    Add first input delay polyfill

--- src/components/seo.js
+++ src/components/seo.js
@@ -10,6 +10,8 @@ import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
+import "first-input-delay/dist/first-input-delay.min.js"
+
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
     graphql`
