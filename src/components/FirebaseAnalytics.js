31d3d76c424065c09bb7044ce9ad758fe41f98be
David Wheatley <blokmaster.help@gmail.com>
2019-09-20 03:09:38 +0100

    Remove firebase

--- src/components/FirebaseAnalytics.js
+++
@@ -1,7 +0,0 @@
-import Firebase from "./Firebase"
-
-// Add the Performance Monitoring library
-import "firebase/performance"
-
-// Initialize Performance Monitoring and get a reference to the service
-Firebase.performance()
