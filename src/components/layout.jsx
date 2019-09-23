8bb211cf2a009fc8682a598f1c1a963bc53f2d3c
David Wheatley <blokmaster.help@gmail.com>
2019-09-23 21:27:08 +0100

    Improve footer on small devices

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -5,6 +5,8 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
+import useMediaQuery from "@material-ui/core/useMediaQuery"
+
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
@@ -63,30 +65,11 @@ const Layout = ({ children }) => {
     }
   `)
 
-  return (
-    <MuiThemeProvider theme={theme}>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-      </div>
-      <Paper
-        component="footer"
-        style={{
-          position: "fixed",
-          bottom: 0,
-          padding: theme.spacing(1.5),
-          paddingBottom: theme.spacing(1.75),
-          width: "100vw",
-        }}
-        elevation={4}
-      >
+  const SmallScreen = useMediaQuery("(min-width:600px)")
+
+  const FooterContent = (
+    <>
+      {SmallScreen ? (
         <Box textAlign="center">
           <Body2>
             &copy; {new Date().getFullYear()}
@@ -97,7 +80,65 @@ const Layout = ({ children }) => {
             </Link>
           </Body2>
         </Box>
-      </Paper>
+      ) : (
+        <Box textAlign="center">
+          <Body2 style={{ fontSize: 13 }}>
+            &copy; {new Date().getFullYear()}
+            {"   "}|{"   "}
+            <Link to="https://github.com/davwheat">
+              <GitHubIcon size={18} className="footer-icon" />
+              davwheat
+            </Link>
+          </Body2>
+        </Box>
+      )}
+    </>
+  )
+
+  return (
+    <MuiThemeProvider theme={theme}>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0px 1.0875rem 1.45rem`,
+          paddingTop: 0,
+          marginBottom: theme.spacing(3),
+        }}
+      >
+        <main>{children}</main>
+      </div>
+
+      {SmallScreen ? (
+        <Paper
+          component="footer"
+          style={{
+            position: "fixed",
+            bottom: 0,
+            padding: theme.spacing(1.5),
+            paddingBottom: theme.spacing(1.75),
+            width: "100vw",
+          }}
+          elevation={4}
+        >
+          {FooterContent}
+        </Paper>
+      ) : (
+        <Paper
+          component="footer"
+          style={{
+            position: "fixed",
+            bottom: 0,
+            padding: theme.spacing(1),
+            paddingBottom: theme.spacing(1.25),
+            width: "100vw",
+          }}
+          elevation={8}
+        >
+          {FooterContent}
+        </Paper>
+      )}
     </MuiThemeProvider>
   )
 }
