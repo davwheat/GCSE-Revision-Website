1cc92ae3ec6499f5f6bd9c7a1229663ed23d3644
David Wheatley <blokmaster.help@gmail.com>
2019-09-23 22:26:42 +0100

    fix favicon; add service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,14 +21,14 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#303030`,
+        theme_color: `#ff9800`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
