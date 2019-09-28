f63b284c43b8ceffd89bde630e833ee8684d8546
David Wheatley <blokmaster.help@gmail.com>
2019-09-28 18:09:54 +0100

    Add react hot loader

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 /**
  * Implement Gatsby's Node APIs in this file.
  *
@@ -5,3 +6,13 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.onCreateWebpackConfig = ({ getConfig, stage }) => {
+  const config = getConfig()
+  if (stage.startsWith("develop") && config.resolve) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "react-dom": "@hot-loader/react-dom",
+    }
+  }
+}
