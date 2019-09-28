0e06745e0f342dc433ff3bd03a6001cfdd348150
David Wheatley <blokmaster.help@gmail.com>
2019-09-28 21:41:48 +0100

    Add custom monospace font

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -13,6 +13,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import "typeface-poppins"
 import "typeface-work-sans"
+import "typeface-fira-mono"
 
 import Header from "./header"
 import { Box, createMuiTheme, MuiThemeProvider, Paper } from "@material-ui/core"
