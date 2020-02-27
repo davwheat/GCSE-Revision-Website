37311c8dadf5527621c75ceb015f7d834cb4d323
David Wheatley <davidwheatley03@gmail.com>
2020-02-27 07:28:36 +0000

    🐛 Fix broken ACC themes article list

--- src/pages/subjects/english/english-literature/topics/a-christmas-carol/themes.jsx
+++ src/pages/subjects/english/english-literature/topics/a-christmas-carol/themes.jsx
@@ -6,23 +6,23 @@ import Breadcrumbs from "../../../../../../components/Breadcrumbs"
 import { H1 } from "../../../../../../components/EasyText"
 import ArticleList from "../../../../../../templates/ArticleList"
 
-const EnglishLitMacbethThemesArticleList = () => {
+const EnglishLitACCThemesArticleList = () => {
   return (
     <Layout>
-      <SEO title="English Literature: Macbeth Themes" />
+      <SEO title="English Literature: A Christmas Carol Themes" />
       <Breadcrumbs
         items={[
           { label: "Home", href: "" },
           { label: "English", href: "subjects/english" },
           { label: "Literature", href: "subjects/english/english-literature" },
-          { label: "Macbeth", href: "subjects/english/english-literature/topics/macbeth" },
+          { label: "A Christmas Carol", href: "subjects/english/english-literature/topics/a-christmas-carol" },
           { label: "Themes" },
         ]}
       />
-      <H1 gutterBottom>English Literature: Macbeth Themes</H1>
-      <ArticleList subject="english literature" topic="macbeth" subtopic="themes" />
+      <H1 gutterBottom>English Literature: A Christmas Carol Themes</H1>
+      <ArticleList subject="english literature" topic="a christmas carol" subtopic="themes" />
     </Layout>
   )
 }
 
-export default EnglishLitMacbethThemesArticleList
+export default EnglishLitACCThemesArticleList
